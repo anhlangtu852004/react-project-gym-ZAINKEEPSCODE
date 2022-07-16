c8aa8d4d315db782435d7e5dac486d8054423c70
tri <tridesignnm@gmal.com>
2022-07-16 16:09:28 +0700

    het reasons

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -19,7 +19,39 @@ const Reasons = () => {
         <img src={image3} alt="" />
         <img src={image4} alt="" />
       </div>
-      <div className="right-r">right</div>
+      <div className="right-r">
+        <span>some reasi</span>
+        <div>
+          <span className="stroke-text">why</span>
+          <span>choose us</span>
+        </div>
+        <div className="details-r">
+          <div>
+            <img src={tick} alt=""></img>
+            <span>over 140+ expert coachs</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>train master and faster than before</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>free 1 program for new member</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>reliable partner</span>
+          </div>
+        </div>
+        <span style={{ color: "var(--gray)", fontWeight: "normal" }}>
+          our partner
+        </span>
+        <div className="partners">
+          <img src={nb} alt="" />
+          <img src={adidas} alt="" />
+          <img src={nike} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
